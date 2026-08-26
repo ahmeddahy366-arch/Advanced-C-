4e2dbeefc55b5751e16149e062a5039da8c5b044
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-08-27 01:27:21 +0300

    question 1

--- Advanced C#/Program.cs
+++ Advanced C#/Program.cs
@@ -8,3 +8,11 @@
         }
     }
 }
+//Q1: What is a generic class? Why use generics?
+//Code written once that allows you to specify the data type 
+//    (such as numbers, text, or objects) 
+//    later at the time of use, rather than fixing it during writing
+//because 1. Type Safety:
+//2.Code Reusability:
+//3.Performance:
+//4.Clean Code:
