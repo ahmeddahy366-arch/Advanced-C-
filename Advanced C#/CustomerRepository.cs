b3fafa7b756be6ad36bd9819dca180dda048462a
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-08-27 15:56:04 +0300

    question6

---
+++ Advanced C#/CustomerRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advanced_C_
+{
+    #region question6
+    //internal class CustomerRepository : IRepository<Customer>
+    //{
+    //    public List<Customer> All = new List<Customer>();
+    //    public void Add(Customer entity)
+    //    {
+    //        All.Add(entity);
+
+    //    }
+
+    //    public void Delet(Customer entity)
+    //    {
+    //        All.Remove(entity);
+    //    }
+
+    //    public Customer Get(int id)
+    //    {
+    //        foreach (Customer entity in All)
+    //        {
+    //            if (entity.Id == id) return entity;
+    //        }
+    //    }
+    //}
+
+    #endregion
+
+}
---
+++ Advanced C#/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advanced_C_
+{
+    //internal interface IRepository<T>
+    //{
+    //   // public void Add(T entity);
+    //   // public void Delet(T entity);
+    //   //public T Get(int id);
+    //}
+}
